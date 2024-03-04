9e38385b3e2590081e99ce868a41fc3a5ac96615
aniketraj72 <aniketojha72@gmail.com>
2024-03-04 23:12:59 +0530

    complete create listing page functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
+  console.log("req: " + req.cookies.access_token);
+  console.log("in verify token: " + token);
 
   if (!token) return next(errorHandler(401, "Unauthorized"));
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
