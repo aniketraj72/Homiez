419b7dd84055fd1a5382d2edfb949fcb14c3a7d2
aniketraj72 <aniketojha72@gmail.com>
2024-03-12 23:32:51 +0530

    add image slider to the listing page

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-          target : "http://localhost:3000",
-          secure:false
-      } 
+        target: "http://localhost:3000",
+        secure: false,
+      },
     },
     plugins: [react()],
   },
