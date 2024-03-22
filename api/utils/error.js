61b9a7705d4e657528c1a589c165e725f216ffc5
aniketraj72 <aniketojha72@gmail.com>
2024-03-22 15:54:46 +0530

    create search api route

--- api/utils/error.js
+++ api/utils/error.js
@@ -2,6 +2,7 @@ const errorHandler = (statusCode, message) => {
   const error = new Error();
   error.statusCode = statusCode;
   error.message = message;
+  console.log("in errorHandler")
   return error;
 };
 
